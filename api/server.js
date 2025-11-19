5816d6495759776bd7a429f758a5fbcbea570232
cmezzaca <christophermezzacappa818@yahoo.ca>
2025-11-18 19:27:47 -0500

    fixed port issue

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ mongoose.connection.once('open', () => {
 const myRoutes = require('./routes/routes');
 app.use('/api', myRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
